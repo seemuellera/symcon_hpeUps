0d5869bf3c95132721398a650078064f51f1bb5e
seemuellera <andreas.seemueller@gmail.com>
2018-01-16 22:20:04 +0100

    First release

--- HpeUps/libs/test.php
+++ HpeUps/libs/test.php
@@ -3,53 +3,20 @@
 
   require('snmp.php');
 
-  $oid = '.1.3.6.1.2.1.69.1.1.3';
-  $oid = '.1.3.6.1.6.3.15.1.1.4.0';
-
   // test the oid_format function
-  $z = oid_format($oid, OID_TEXT);
-  $zz = oid_format($z, OID_NUMERIC);
-  echo "$oid => $z => $zz\n";
 
-  $ip = '172.16.0.116'; 		// ip address or hostname
-  $ips = array($ip, '172.16.0.64');	// array of ip addresses or hostnames
+  $ip = '10.4.150.200'; 		// ip address or hostname
   $community = 'public';		// community string
-  $oid = '.1.3.6.1.2.1.1';		// only numerical oids are supported
-  $oids = array('.1.3.6.1.2.1.1.1', '.1.3.6.1.2.1.1.3');
+  $oid = '.1.3.6.1.4.1.232.165.3.2.1';
 
   $snmp = new snmp();
 
   $snmp->version = SNMP_VERSION_2;
 
-  print_r($snmp->walk($ip, $oid, ['community' => $community]));
-  print_r($snmp->multi_walk($ips, $oid, ['community' => $community]));
-
-
-  $snmp->version = SNMP_VERSION_3;
-  print_r($snmp->get('localhost', '.1.3.6.1.2.1.1.3.0', array('v3_flags'=>SNMP_AUTH_PRIV, 'v3_user'=>'v3user',
-                                                              'v3_auth'=>'authpassword', 'v3_priv'=>'privpassword')));
-
   // get system uptime
-  print_r($snmp->get($ip, '.1.3.6.1.2.1.1.3.0', ['community' => $community]));
-  print_r($snmp->multi_get($ips, '.1.3.6.1.2.1.1.3.0', ['community' => $community]));
+  print_r($snmp->get($ip, $oid, ['community' => $community]));
+  print_r($snmp->multi_get($ip, $oid, ['community' => $community]));
 
-  // bulk get
-  print_r($snmp->bulk_get($ip, $oids));
+  print_r($snmp->bulk_get($ip, $oid, ['community' => $community]));
 
-  // reset cable modem(s)
-  $oid = '.1.3.6.1.3.83.1.1.4.0.1.1.3.0';
-  $snmp->set($ip, $oid, 1, 'i', ['community' => 'private']);
-  $snmp->multi_set($ips, $oid, 1, 'i', ['community' => 'private']);
-
-  // send a trap
-  $ip = '123.45.12.3';
-  $community = 'public';
-  $varbind = $snmp->build_varbind('.1.3.6.1.3.83.1.1.4.1', 17, 'i');
-  $enterprise = '.1.3.6.1.3.83.1.1.4.0.1.1.3.0';
-  $agent = '127.0.0.1';
-  $trap_type = TRAP_LINKUP;
-  $specific_trap_type = 2;
-  $uptime = 123;
-
-  $snmp->trap($ip, $community, $varbind, $enterprise, $agent, $trap_type, $specific_trap_type, $uptime);
 ?>
--- HpeUps/module.php
+++ HpeUps/module.php
@@ -28,44 +28,40 @@
 		$this->RegisterPropertyInteger("RefreshInterval",5);
 
 		// Variables
-		$this->RegisterVariableString("BotvacSerial", "Robot Serial Number");
-		$this->RegisterVariableString("BotvacModel", "Robot Model");
-		$this->RegisterVariableString("BotvacName", "Robot Name");
-		$this->RegisterVariableString("BotvacFirmware", "Robot Firmware Version");
-		$this->RegisterVariableString("BotvacExtStatus", "Robot Extended Status");
-		$this->RegisterVariableBoolean("BotvacStatus", "Robot Power Status","~Switch");
-		$this->RegisterVariableBoolean("BotvacEcoMode", "Robot Eco Mode", "~Switch");
-		$this->RegisterVariableBoolean("BotvacDocked", "Robot is Docked");
-		$this->RegisterVariableInteger("BotvacCharge", "Battery Level", "~Intensity.100");
+		$this->RegisterVariableString("HpeUpsMgmtFW", "Management Module Firmware Version");
+		$this->RegisterVariableString("HpeUpsMgmtHW", "Management Module Hardware Version");
+		$this->RegisterVariableString("HpeUpsMgmgPartNr", "Management Module Part Number");
+		$this->RegisterVariableString("HpeUpsMgmgSerialNr", "Management Module Serial Number");
+		$this->RegisterVariableString("HpeUpsSW", "UPS Software Version");
+		$this->RegisterVariableString("HpeUpsModel", "UPS Model");
+		$this->RegisterVariableInteger("HpeUpsBatTimeRemaining", "Remaining battery time");
+		$this->RegisterVariableInteger("HpeUpsBatVoltage", "Battery voltage");
+		$this->RegisterVariableInteger("HpeUpsBatCapacity", "Battery capacity");
+		$this->RegisterVariableInteger("HpeUpsBatAbmStatus", "Battery status");
+		$this->RegisterVariableFloat("HpeUpsInputFrequency", "Input frequency");
+		$this->RegisterVariableInteger("HpeUpsInputLineBads", "Number of times input power was out of tolerance");
+		$this->RegisterVariableInteger("HpeUpsInputVoltage", "Input voltage");
+		$this->RegisterVariableInteger("HpeUpsInputCurrent", "Input current");
+		$this->RegisterVariableInteger("HpeUpsOutputLoad", "Output Load");
+		$this->RegisterVariableFloat("HpeUpsOutputFrequency", "Output frequency");
+		$this->RegisterVariableInteger("HpeUpsOutputVoltage", "Output voltage");
+		$this->RegisterVariableInteger("HpeUpsOutputCurrent", "Output current");
+		$this->RegisterVariableInteger("HpeUpsOutputPower", "Output power");
+		$this->RegisterVariableInteger("HpeUpsOutputSource", "Output source");
+		$this->RegisterVariableInteger("HpeUpsAmbientTemperature", "Ambient temperature");
+		$this->RegisterVariableInteger("HpeUpsBatteryTestStatus", "Battery test status");
 
 		// Timer
-		$this->RegisterTimer("UpdateRobotStatus", 0, 'BOTVAC_RefreshInformation($_IPS[\'TARGET\']);');
+		$this->RegisterTimer("UpdateHpeUpsStatus", 0, 'HPEUPS_RefreshInformation($_IPS[\'TARGET\']);');
  
         }
  
         // Überschreibt die intere IPS_ApplyChanges($id) Funktion
         public function ApplyChanges() {
 
-		// Check if there is a Botvac Secret. If not we need to handle the API authentication
-		if (! $this->ReadPropertyString("BotvacSecret") ) {
-		
-			// If there is no Authentication token we need to generate one first
-			if (! $this->ReadPropertyString("AuthToken") ) {
-			
-				$NeatoClient = new NeatoBotvacClient(false, $this->ReadPropertyString("BotvacVendor") );
-	                        $AuthToken = $NeatoClient->authorize($this->ReadPropertyString("Username"), $this->ReadPropertyString("Password") );
+		$newInterval = $this->ReadPropertyInteger("RefreshInterval") * 1000;
+	        $this->SetTimerInterval("RefreshInformation", $newInterval);
 
-				if (! $AuthToken) {
-				
-					IPS_LogMessage($_IPS['SELF'],"BOTVAC - Authentication with username and password was not possible");
-				}
-				else {
-				
-					IPS_SetProperty($_IPS['SELF'],"AuthToken",$AuthToken);
-				}
-			}
-		}
-		
 
             // Diese Zeile nicht löschen
             parent::ApplyChanges();
@@ -81,95 +77,13 @@
 			"actions" => Array()
         		);
 
-		// If we do not have a secret we also have no robot information. So we need to display formst to ask for that first.
-		if ( ! $this->ReadPropertyString("BotvacSecret") ) {
-
-			// If there is no username and password stored we need to ask for it
-			if ( ! ( ($this->ReadPropertyString("Username") ) && (! $this->ReadProperty("Password") ) ) ) {
-		
-				$form['elements'][] = Array("type" => "Label", "label" => "No robot API secret is stored in the module. Please provide your account details to retrieve them:");
-				$form['elements'][] = Array("type" => "ValidationTextBox", "name" => "Username", "caption" => "Username");
-				$form['elements'][] = Array("type" => "PasswordTextBox", "name" => "Password", "caption" => "Password");
-			
-				// Now we need an array of vendors
-				$BotvacVendorOptions = Array();
-				$BotvacVendorOptions[] = Array("label" => "Neato", "value" => "neato");
-				$BotvacVendorOptions[] = Array("label" => "Vorwerk", "value" => "vorwerk");
-				$form['elements'][] = Array("type" => "Select", "name" => "BotvacVendor", "caption" => "Select Vendor", "options" => $BotvacVendorOptions);
-
-				$form['elements'][] = Array("type" => "Label", "label" => "Please click on Apply to retrieve a list of robots");
-
-				// Stop processing for now and return the form
-				return json_encode($form);
-			}
-		}
-
-		if (! $this->GetBuffer('AuthToken') ) {
-
-
-			$NeatoClient = new NeatoBotvacClient(false, $this->ReadPropertyString("BotvacVendor") );
-			$AuthToken = $NeatoClient->authorize($this->ReadPropertyString("Username"), $this->ReadPropertyString("Password") );
-
-                        if ($AuthToken) {
-
-                                $this->SetBuffer('AuthToken', $AuthToken);
-                        }
-
-		}
-		else {
-
-			$NeatoClient = new NeatoBotvacClient($this->GetBuffer('AuthToken'), $this->ReadPropertyString("BotvacVendor") );
-
-			$form['elements'][] = Array("type" => "Label", "label" => "An Authentication Token was found");
-
-			// Now we need a list of robots
-			$robots = Array();
-			$robotSelectOptions = Array();
-
-			$result = $NeatoClient->getRobots();
-
-			$robots = $result['robots'];
-
-			foreach ($robots as $robot) {
-				
-				$robotSelectOptions[] = Array("label" => $robot['name'], "value" => $robot['serial']);
-			}
-
-			$form['elements'][] = Array("type" => "Select", "name" => "Robot", "caption" => "Select Robot", "options" => $robotSelectOptions);
-
-
-			// Fill the variables when a Robot is selcted
-			if ($this->ReadPropertyString("Robot") ) {
-
-				SetValue($this->GetIDForIdent("BotvacSerial"), $this->ReadPropertyString("Robot") );
-
-				foreach ($robots as $currentRobot) {
-
-					if ($currentRobot['serial'] == $this->ReadPropertyString("Robot")) {
-
-						SetValue($this->GetIDForIdent("BotvacSecret"), $currentRobot["secret_key"]);
-						SetValue($this->GetIDForIdent("BotvacName"), $currentRobot["name"]);
-						SetValue($this->GetIDForIdent("BotvacModel"), $currentRobot["model"]);
-					}
-				}
-			}
-
-		}
-
-		if (GetValue($this->GetIDForIdent("BotvacSecret") ) != "" ) {
-	
-			$form['elements'][] = Array("type" => "Label", "label" => "A Robot with name " . GetValue($this->GetIDForIdent("BotvacName")) .  " and serial number " . GetValue($this->GetIDForIdent("BotvacSerial")) . " was successfully registered");
-		}
-	
-		// Add a number spinner to select the refresh cycle
-		$form['elements'][] = Array("type" => "NumberSpinner", "name" => "RefreshInterval", "caption" => "Select Refresh Interval");
+		// Add the Elements
+		$form['elements'][] = Array("type" => "NumberSpinner", "name" => "RefreshInterval", "caption" => "Refresh Interval");
+		$form['elements'][] = Array("type" => "ValidationTextBox", "name" => "Hostname", "caption" => "Hostname");
+		$form['elements'][] = Array("type" => "PasswordTextBox", "name" => "Community", "caption" => "Community");
 
 		// Add the buttons for the test center
-		$form['actions'][] = Array("type" => "Button", "label" => "Refresh Robot Data", "onClick" => 'BOTVAC_RefreshInformation($id);');
-		$form['actions'][] = Array("type" => "Button", "label" => "Start Cleaning", "onClick" => 'BOTVAC_StartCleaning($id);');
-		$form['actions'][] = Array("type" => "Button", "label" => "Pause Cleaning", "onClick" => 'BOTVAC_PauseCleaning($id);');
-		$form['actions'][] = Array("type" => "Button", "label" => "Resume Cleaning", "onClick" => 'BOTVAC_ResumeCleaning($id);');
-		$form['actions'][] = Array("type" => "Button", "label" => "Send to Base", "onClick" => 'BOTVAC_SendToBase($id);');
+                $form['actions'][] = Array("type" => "Button", "label" => "Refresh Overall Status", "onClick" => 'HPEUPS_RefreshInformation($id);');
 
 		// Return the completed form
 		return json_encode($form);
@@ -183,66 +97,19 @@
         */
         public function RefreshInformation() {
 
-		$NeatoRobot = new NeatoBotvacRobot(GetValue($this->GetIDForIdent("BotvacSerial")), GetValue($this->GetIDForIdent("BotvacSecret")), GetValue($this->GetIDForIdent("BotvacModel")) );	
+		SetValue($this->GetIDForIdent("HpeUpsMgmtFW"),$this->SnmpGet('.1.3.6.1.4.1.232.165.1.2.3');
+	}
 
-		$robotInformation = $NeatoRobot->getState();
-
-		SetValue($this->GetIDForIdent("BotvacFirmware"), $robotInformation["meta"]["firmware"]);
-		SetValue($this->GetIDForIdent("BotvacDocked"), $robotInformation["details"]["isDocked"]);
-		SetValue($this->GetIDForIdent("BotvacCharge"), $robotInformation["details"]["charge"]);
-
-		if ($robotInformation["details"]["isDocked"] == 1) {
-
-			SetValue($this->GetIDForIdent("BotvacExtStatus"), "In Docking Station");
-		}
-        }
-
-	public function StartCleaning() {
-
-		$NeatoRobot = new NeatoBotvacRobot(GetValue($this->GetIDForIdent("BotvacSerial")), GetValue($this->GetIDForIdent("BotvacSecret")), GetValue($this->GetIDForIdent("BotvacModel")) );	
-
-		// Check if EcoMode is activated
-		if (GetValue($this->GetIDForIdent("BotvacEcoMode") ) ) {
-
-			$NeatoRobot->startEcoCleaning();
-			SetValue($this->GetIDForIdent("BotvacExtStatus"), "Cleaning in Eco-Mode");
-		}
-		else {
+	protected function SnmpGet($oid) {
 	
-			$NeatoRobot->startCleaning();
-			SetValue($this->GetIDForIdent("BotvacExtStatus"), "Cleaning");
-		}
-	}
+		$snmp = new snmp();
+		$snmp->version = SNMP_VERSION_2;
 
-	public function PauseCleaning() {
+		$result = $snmp->bulk_get($this->ReadPropertyString("Hostname"), $oid, ['community' => $this->ReadPropertyString("Community") ] );
 
-		$NeatoRobot = new NeatoBotvacRobot(GetValue($this->GetIDForIdent("BotvacSerial")), GetValue($this->GetIDForIdent("BotvacSecret")), GetValue($this->GetIDForIdent("BotvacModel")) );	
-		$NeatoRobot->pauseCleaning();
-		SetValue($this->GetIDForIdent("BotvacExtStatus"), "Paused");
-	}
+		$resultValue = $result[$oid];
 
-	public function ResumeCleaning() {
-
-		$NeatoRobot = new NeatoBotvacRobot(GetValue($this->GetIDForIdent("BotvacSerial")), GetValue($this->GetIDForIdent("BotvacSecret")), GetValue($this->GetIDForIdent("BotvacModel")) );	
-
-		// Check if EcoMode is activated
-		if (GetValue($this->GetIDForIdent("BotvacEcoMode") ) ) {
-
-			$NeatoRobot->resumeCleaning();
-			SetValue($this->GetIDForIdent("BotvacExtStatus"), "Cleaning in Eco-Mode");
-		}
-		else {
-	
-			$NeatoRobot->resumeCleaning();
-			SetValue($this->GetIDForIdent("BotvacExtStatus"), "Cleaning");
-		}
-	}
-
-	public function SendToBase() {
-
-		$NeatoRobot = new NeatoBotvacRobot(GetValue($this->GetIDForIdent("BotvacSerial")), GetValue($this->GetIDForIdent("BotvacSecret")), GetValue($this->GetIDForIdent("BotvacModel")) );	
-		$NeatoRobot->sendToBase();
-		SetValue($this->GetIDForIdent("BotvacExtStatus"), "Back to Base");
+		return $resultValue;
 	}
 
     }
